7614f9c99a8152c1b7b8bd15b53edbdf7f0f8da4
张元泽 <zhangyuanze2850@yundasys.com>
2022-11-13 17:12:11 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <n-space>
+      <n-button type="error"> Error </n-button>
+      <n-button type="error" @click="handleAddEvent"> {{ count }} </n-button>
+    </n-space>
+
+    <TheWelcome />
+  </main>
+</template>
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue';
+const count = ref(1);
+const handleAddEvent = () => {
+  count.value++;
+};
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+        },
+      ],
+      dirs: [],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dirs: [],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
